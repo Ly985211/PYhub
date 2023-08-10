7413c29608a2adde98f58147ddecba309c9db089
ly <ly002023@126.com>
2023-08-10 18:33:56 +0800

    setdefault and missing:for dict

---
+++ learningPY/part1and2/missing.py
@@ -0,0 +1,29 @@
+class Strkey(dict):
+
+    def __missing__(self,key):
+        if isinstance(key,str):
+            raise KeyError(key)
+        return self[str(key)]
+    
+    def get(self,key,default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+        
+    def __contains__(self, key):
+        return key in self.keys() or str(key) in self.keys()
+
+if __name__=="__main__":
+    d=Strkey([('2','two'),('3','three')])#dict_keys(['2', '3'])
+    print(d.keys())
+    print(d[2]) #two
+    print(d[3]) #three
+    #print(d[4]) -> KeyError: '4'
+
+    print(d.get('2'))#two
+    print(d.get(3))#three
+    #print(d.get(4)) ->    #nothing
+    
+    print(2 in d) #True
+    #print(4 in d) -> False
---
+++ learningPY/part1and2/setdefault.py
@@ -0,0 +1,14 @@
+dict1={1:['a1']}
+print(dict1)# {1: ['a1']}
+dict1[3]=['c1']
+print(dict1)# {1: ['a1'], 3: ['c1']}
+key,value=1,'a2'
+dict1.setdefault(key,[]).append(value)
+print(dict1)# {1: ['a1', 'a2'], 3: ['c1']}
+key,value=2,'b1'
+dict1.setdefault(key,[]).append(value)
+print(dict1)# {1: ['a1', 'a2'], 3: ['c1'], 2: ['b1']}
+
+
+
+
